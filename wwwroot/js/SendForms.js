3631c2b769c1ad40ae98dc7793e188d36b92f2e0
Mend1s <andremendesskt99@gmail.com>
2025-10-16 23:40:19 -0300

    add forms

---
+++ wwwroot/js/SendForms.js
@@ -0,0 +1,10 @@
+﻿window.SendForms = function (serviceId, templateId, formData) {
+    return emailjs.send(serviceId, templateId, formData)
+        .then(function (response) {
+            console.log("✅ Email enviado!", response.status, response.text);
+            return true;
+        }, function (error) {
+            console.error("❌ Erro ao enviar:", error);
+            return false;
+        });
+}
